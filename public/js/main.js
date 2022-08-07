3bb83d93319f53f729cc8ed33ea96d39c190845c
JvJev <jevgenij.voronov@gmail.com>
2022-08-08 00:32:31 +0300

    feat 1-4 homework

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+import { pirmaUzduotis, antraUzduotis } from './functionsTask.js';
+import { objektai } from './objects.js';
+import { exportAllArrays } from './arrays.js';
+import { pirmaPaskaitaAntraUzduotis } from './primitive-types.js';
+import { treciaPaskaita } from './treciosPaskaitosUzduotys.js';
+console.log('pirmos paskaitos uzduotys');
+console.log('');
+import { tipuAparsymu16Uzduociu } from './tipų_aprašymo_užtvirtinimas.js';
+import { isplestiniaiTipai } from './isplestiniai_tipai.js';
+isplestiniaiTipai();
+pirmaUzduotis();
+antraUzduotis();
+objektai();
+exportAllArrays();
+pirmaPaskaitaAntraUzduotis();
+treciaPaskaita();
+tipuAparsymu16Uzduociu();
+//# sourceMappingURL=main.js.map
